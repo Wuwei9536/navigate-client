d44bd1c4c7298abc48e8e8c43c19bbbfcb2eab82
吴伟01 <wien.wuwei@gmail.com>
2019-06-25 11:19:00 +0800

    feat: icon

--- src/layout/home.tsx
+++ src/layout/home.tsx
@@ -3,45 +3,45 @@ import { Layout, Menu, Icon } from 'antd';
 import styles from './home.module.less';
 import { IcardData } from '../stores/home/index';
 
-
 const { Sider } = Layout;
+const IconFont = Icon.createFromIconfontCN({
+	scriptUrl: '//at.alicdn.com/t/font_1259557_zsmza8iko9.js'
+});
 interface Iprops {
-  menuInfo: IcardData[]
+	menuInfo: IcardData[];
 }
-export default class HomeLayout extends React.Component<Iprops>{
-  state = {
-    collapsed: false,
-  };
+export default class HomeLayout extends React.Component<Iprops> {
+	state = {
+		collapsed: false
+	};
 
-  toggle = () => {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
-  };
+	toggle = () => {
+		this.setState({
+			collapsed: !this.state.collapsed
+		});
+	};
 
-  render() {
-    const { menuInfo } = this.props;
-    console.log('%cmenuInfo: ', 'font-size:15px;background-color: rgb(135, 208, 104);', menuInfo);
-    return (
-      <Layout className={styles.layout}>
-        <Sider trigger={null} collapsible collapsed={this.state.collapsed} className={styles.sider}>
-          <div className={styles.logo}>WEB 前端导航</div>
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['0']}>
-            {menuInfo.map((item,index) => {
-              const { menuIcon, menuName } = item;
-              return (
-                <Menu.Item key={index}>
-                  <Icon type="user" />
-                  <span>{menuName}</span>
-                </Menu.Item>
-              )
-            })}
-          </Menu>
-        </Sider>
-        <Layout style={{paddingBottom:20}}>
-          {this.props.children}
-        </Layout>
-      </Layout>
-    );
-  }
+	render() {
+		const { menuInfo } = this.props;
+		console.log('%cmenuInfo: ', 'font-size:15px;background-color: rgb(135, 208, 104);', menuInfo);
+		return (
+			<Layout className={styles.layout}>
+				<Sider trigger={null} collapsible collapsed={this.state.collapsed} className={styles.sider}>
+					<div className={styles.logo}>WEB 前端导航</div>
+					<Menu theme="dark" mode="inline" defaultSelectedKeys={[ '0' ]}>
+						{menuInfo.map((item, index) => {
+							const { menuIcon, menuName } = item;
+							return (
+								<Menu.Item key={index}>
+									<IconFont type={menuIcon} />
+									<span>{menuName}</span>
+								</Menu.Item>
+							);
+						})}
+					</Menu>
+				</Sider>
+				<Layout style={{ paddingBottom: 20 }}>{this.props.children}</Layout>
+			</Layout>
+		);
+	}
 }
